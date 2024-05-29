4f802f6d275d89036d9c001e0ad5c065b1b94394
sara <matika20@gmail.com>
2024-05-29 23:11:40 +0200

    implementing the exception controller class

--- src/main/java/controllers/ExceptionController.java
+++ src/main/java/controllers/ExceptionController.java
@@ -1,3 +1,16 @@
 package controllers;
 
-public class ExceptionController {}
+import exceptions.RecordNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+    @ExceptionHandler(value = RecordNotFoundException.class)
+    public ResponseEntity<String> exception(RecordNotFoundException exception)
+    {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
